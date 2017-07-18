44d9d88c8e4e5f288ee618a7b1416642bde18a5d
jihanemastane <mastanejihane0@gmail.com>
2017-07-18 16:56:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+/*  Titre : Mini Projet gestion de stock     Auteurs: Mastane Jihane- Meskali Reda     Encadrée par Pr.MILOUD LAHMAIM        */
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+#include <errno.h>
+#include "Menu.h"
+
+
+int main()
+{
+
+  MenuPrincipale();
+
+
+
+}
